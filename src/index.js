c90b0084ebe9917da712ab15fba0d79cdffc76ff
wremsen <wcremsen@gmail.com>
2023-12-14 10:17:35 -0500

    setup finish

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import Example from "./scripts/example";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const main = document.getElementById("main");
+//     new Example(main);
+//     console.log("hello world");
+// })
+
+
+
+import Example from "./scripts/example";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById("main");
+    // debugger;
+    new Example(main);
+    console.log("hello world");
+});
