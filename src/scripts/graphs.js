e49438e3bc9f5abeecf593e933a492075ba21584
wremsen <wcremsen@gmail.com>
2023-12-17 16:47:04 -0500

    basic magnitude chart

---
+++ src/scripts/graphs.js
@@ -0,0 +1,43 @@
+const proxima = {
+    absolute_magnitude: "15.49",
+    apparent_magnitude: "11.05",
+    constellation: "Centaurus",
+    declination: "−62° 40′ 46.1″",
+    distance_light_year: "4.22",
+    name: "Proxima Centauri",
+    right_ascension: "14h 29m 42.95s",
+    spectral_class: "M5.5V"
+}
+
+const magnitudeGraph = document.getElementById("magnitudeChart");
+
+const magnitudeData = {
+  labels: ["Apparent Magnitude", "Absolute Magnitude"],
+  datasets: [{
+    data: [11.05, 15.49],
+    backgroundColor: 'white',
+    borderColor: 'White',
+    borderWidth: 10
+  }]
+}
+
+const barOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+      max: 20,
+      ticks: {
+        color: 'white',
+      }
+    }
+  },
+  plugins: {
+    legend: {
+      display: false,
+    }
+  },
+  responsive: false
+}
+
+let magnitude = new Chart(magnitudeGraph, {type: 'bar', data: magnitudeData, options: barOptions})
+  
