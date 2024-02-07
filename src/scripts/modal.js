629354c3354b361700715fbda9986d13846a0e65
wremsen <wcremsen@gmail.com>
2024-02-07 10:35:22 -0500

    personal links

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const openMod = document.getElementById("mainTitle");
     const closeMod = document.getElementById("closeMod");
     const modal = document.getElementById("modContainer");
+    const linkedIn = document.getElementById("liLogo")
+    const gitHub = document.getElementById("ghLogo")
 
     openMod.addEventListener('click', function () {
         modal.style.display = "flex";
@@ -9,5 +11,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     closeMod.addEventListener('click', function() {
         modal.style.display = "none";
+    })
+
+    linkedIn.addEventListener('click', function() {
+        window.location.href = "https://www.linkedin.com/in/billy-remsen-b0969a120/"
+    })
+
+    gitHub.addEventListener('click', function() {
+        window.location.href = "https://github.com/wremsen"
     })
 })
